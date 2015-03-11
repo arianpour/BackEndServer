6e474133912b5cd8f706a53821dd78b335e16a59
arian <arianpourinfo@gmail.com>
2015-03-11 14:52:49 -0700

    added TODO

--- src/main/java/FrontEnd/main.java
+++ src/main/java/FrontEnd/main.java
@@ -10,32 +10,38 @@ class main {
     public static void main(String[] args) {
 
         //get the movie name and set it into JSONCotroller for process
+        //TODO: change the movie input to user search
         String moviename=getInput("Enter the movie Name : ");
         JSONController m=new JSONController(moviename);
        //return movie object if exist and return null
         List<MOVIEObject> mObjects = m.controller();
         if(mObjects!=null) {
+            //TODO: user UI changer
             for (int i = 0; i < mObjects.size(); i++) {
                 System.out.println(i+"."+mObjects.get(i).getName() + "=>" + mObjects.get(i).getThumbail()
                         + "=>" + mObjects.get(i).getYear() + "=>" + mObjects.get(i).getAudience_score());
             }
         }
+        //insert data into database after checking no duplicate
         dbHandler mObj=new dbHandler();
         if (mObjects != null) {
+            //TODO: change user input after connect to user interface
             int choice=Integer.parseInt(getInput("Choose movie you want "));
             if (!mObj.duplicateChecker(mObjects.get(choice).getName(),mObjects.get(choice).getYear())){
             mObj.movieInserter(mObjects.get(choice));
+                //TODO: user UI changer
             messagePrinter("The new Item already inserted : "+mObjects.get(choice).getName());}
             else {
+                //TODO: user UI changer
                 messagePrinter("The movie is already in Database");
             }
         }
     }
-
+    //TODO: user UI changer
     private static void messagePrinter(String message) {
         System.out.println(message);
     }
-
+    //TODO: user UI changer
     protected static String getInput(String message){
         Scanner input=new Scanner(System.in);
         System.out.println(message);
